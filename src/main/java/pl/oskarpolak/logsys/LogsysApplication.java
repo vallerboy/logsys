4081154c5d8a74a6c4703b4fcd619816013834c5
Oskar Polak <biuro@fivarto.pl>
2018-03-19 10:03:29 +0100

    Initial commit

---
+++ src/main/java/pl/oskarpolak/logsys/LogsysApplication.java
@@ -0,0 +1,12 @@
+package pl.oskarpolak.logsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsysApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/oskarpolak/logsys/LogsysApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.oskarpolak.logsys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogsysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
